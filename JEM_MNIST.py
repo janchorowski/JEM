beac47128ee2c6ec106edeeea9d650bfed6025b1
Stephen Zhao <szstephenzhao88@gmail.com>
2020-02-18 11:29:21 -0500

    update

--- JEM_MNIST.py
+++ JEM_MNIST.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision import transforms
+import numpy as np
 
 
 train_dataset = torchvision.datasets.MNIST(root='./data',train=True, transform=transforms.ToTensor(), download=True)
@@ -16,27 +17,32 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=bat
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,  batch_size=batch_size, shuffle=False)
 
 class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, extra_layers=0):
         super(NeuralNet, self).__init__()
         self.layers = nn.ModuleList()
 
         layer_in = nn.Linear(input_size, hidden_size)
         self.layers.append(layer_in)
-        for i in range(0):
+        for i in range(extra_layers):
             self.layers.append(nn.Linear(hidden_size, hidden_size))
         self.layer_out = nn.Linear(hidden_size, output_size)
         # self.layers.append(layer_out)
 
         self.relu = nn.ReLU()
 
-    def forward(self, x):
+    def forward(self, x, y=None):
         for layer in self.layers:
             x = layer(x)
             x = self.relu(x)
-        # output = self.layer1(x)
-        # output = self.relu(output)
-        # output = self.layer2(output)
-        output = self.layer_out(x)
+        logits = self.layer_out(x)
+        # if y is None:
+        #     output = logits.logsumexp(dim=1)
+        #     print(logits.shape)
+        #     print(output.shape)
+        # else:
+        #     print(y[:,None])
+        #     output = torch.gather(logits, 1, y[:,None])
+        output = logits
         return output
 
 
@@ -44,23 +50,47 @@ input_size = 784
 hidden_size = 500
 output_size = 10
 num_epochs = 5
+image_size = 28
 
 learning_rate = 0.002
 
 model = NeuralNet(input_size, hidden_size, output_size)
+f = model
 
 # CE loss below has logsoftmax (logsumexp?) and neg log lik already together
-lossFunction = nn.CrossEntropyLoss()
+p_y_given_x_loss = nn.CrossEntropyLoss()
 # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+#
+
+def sgld(steps: int, step_size: int = 2, ):
+    # Draws a sample according to SGLD, starting from uniform distribution sample
+    # 0,1 as uniform bounds for MNIST
+    alpha = step_size
+    x_0 = torch.FloatTensor(batch_size, image_size * image_size).uniform_(0, 1)
+    x_i = x_0
+    for step in range(steps):
+        eps = torch.FloatTensor(batch_size, image_size * image_size).normal_(0, np.sqrt(alpha))
+        x_i = torch.tensor(x_i, requires_grad=True)
+        # print(f(x_i))
+        # print(f(x_i, y=torch.randint(0, 10, (batch_size,))))
+        de_dtheta = torch.autograd.grad(outputs=-f(x_i).logsumexp(dim=1).sum(), inputs=x_i)[0]
+        # print(alpha)
+        # print(de_dtheta)
+        x_i1 = x_i - alpha/2 * de_dtheta + eps #logsumexp gradient here, with respect to (input?)
+        x_i = x_i1
+    return x_i
+
+sgld(10)
 
 total_step = len(train_loader)
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
-        images = images.reshape(-1, 28 * 28)
+        images = images.reshape(-1, image_size * image_size)
         out = model(images)
-        loss = lossFunction(out, labels)
+        loss = p_y_given_x_loss(out, labels)
+        # print(images[0])
 
         optimizer.zero_grad()
         loss.backward()
@@ -76,7 +106,7 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_loader:
-        images = images.reshape(-1, 28*28)
+        images = images.reshape(-1, image_size*image_size)
         out = model(images)
         _, predicted = torch.max(out.data,1)
         total += labels.size(0)
